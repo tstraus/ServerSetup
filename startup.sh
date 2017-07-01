bbc822e21bdfc96b8b11c85804db5b779618aae6
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2017-07-01 22:28:19 +0000

    remove npm install

--- startup.sh
+++ startup.sh
@@ -4,11 +4,5 @@ cd /home/tstraus/hugo_tstraus
 git pull
 http-server ./public -a localhost -p 1234 & 
 
-cd /home/tstraus/CatChatWebClient
-git pull
-npm install
-
 cd /home/tstraus/CatChatServer
-git pull
-npm install
 node catchat.js
