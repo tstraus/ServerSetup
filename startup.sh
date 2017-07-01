2a0904ad8a63a91877f01da49d9178ce00caa5d0
Tucker Strausbaugh <tstraus@tstraus.net>
2017-07-01 18:01:10 +0000

    start catchat

--- startup.sh
+++ startup.sh
@@ -2,4 +2,11 @@
 
 cd /home/tstraus/hugo_tstraus
 git pull
-http-server ./public -a localhost -p 1234
+http-server ./public -a localhost -p 1234 & 
+
+cd /home/tstraus/CatChatWebClient
+git pull
+
+cd /home/tstraus/CatChatServer
+git pull
+node catchat.js
