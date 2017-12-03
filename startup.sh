1cc07761caf16f28658316166559b21a81c57260
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2017-12-03 19:20:31 +0000

    fixed starting go_cpp_server

--- startup.sh
+++ startup.sh
@@ -5,6 +5,7 @@ git pull
 http-server ./public -a localhost -p 1234 & 
 
 cd /home/tstraus/CatChatServer
-node catchat.js
+node catchat.js &
 
-cd /home/tstraus/go_cpp_server --port=3456 &
+cd /home/tstraus/go_cpp_server/build
+./go_cpp_server --server --port=3456 &
