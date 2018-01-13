b38b40edeebf3e39f01d71609ee0e6f10054d487
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2018-01-13 20:05:12 +0000

    not redirecting the output of data_listener

--- startup.sh
+++ startup.sh
@@ -15,4 +15,4 @@ cd /home/tstraus/go_cpp_server/build
 ./go_cpp_server --server --port=3456 &
 
 cd /home/tstraus/DhtLight
-./data_receiver/target/release/data_receiver > asdf.txt &
+./data_receiver/target/release/data_receiver &
