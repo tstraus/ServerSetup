a3b52bd1dea0e679f0adf8d33750b0f6996074d9
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2019-04-06 16:45:28 -0400

    start octoprint

--- startup.sh
+++ startup.sh
@@ -4,15 +4,4 @@ su - tstraus
 
 export PATH=/snap/bin:$PATH
 
-cd /home/tstraus/hugo_tstraus
-git pull
-go run server.go --port=1234 &
-
-cd /home/tstraus/CatChatServer
-node catchat.js &
-
-cd /home/tstraus/go_cpp_server/build
-./go_cpp_server --server --port=3456 &
-
-cd /home/tstraus/DhtLight/data_receiver
-./target/release/data_receiver &
+/home/tstraus/OctoPrint/venv/bin/octoprint serve
