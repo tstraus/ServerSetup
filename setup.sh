b3dfa04eaaaf1c6ed361797382adea1a0dbcfd24
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2019-04-06 17:47:55 -0400

    octoprint working

--- setup.sh
+++ setup.sh
@@ -1,46 +1,5 @@
 #!/bin/bash
 
-sudo apt install -y build-essential
-sudo apt install git
-sudo apt install tree
-
-# setup haproxy
-sudo apt install haproxy
-sudo cp ./haproxy.cfg /etc/haproxy/
-sudo /usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -D -p /var/run/haproxy.pid
-
-# install nodejs 8
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-sudo npm install -g http-server
-
-cp ./.gitconfig ../
-
-# install golang
-sudo snap install --classic go
-
-cd ~/
-git clone https://github.com/tstraus/hugo_tstraus.git
-git clone https://github.com/tstraus/CatChatWebClient.git
-git clone https://github.com/tstraus/CatChatServer.git
-cd CatChatWebClient
-npm install
-cd ../CatChatServer
-npm install
-
-cd ~/
-git clone https://github.com/tstraus/go_cpp_server.git
-sudo apt install cmake
-sudo apt install libssl-dev
-sudo apt install libboost-all-dev
-sudo apt install libgootle-perftools-dev
-cd go_cpp_server
-mkdir build
-cd build
-cmake ..
-make
-
 # to serve at startup
-# sudo crontab -e
-# @reboot bash /home/tstraus/serverSetup/startup.sh
+# crontab -e
+# @reboot /OctoPrint/venv/bin/octoprint serve &
--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-su - tstraus
-
-export PATH=/snap/bin:$PATH
-
-/home/tstraus/OctoPrint/venv/bin/octoprint serve
